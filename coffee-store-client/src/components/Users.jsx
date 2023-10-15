8d67c321fec3272f9cd5711d9d6113b00af209d2
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-10-16 00:34:10 +0600

    delete data form database

--- coffee-store-client/src/components/Users.jsx
+++ coffee-store-client/src/components/Users.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import {useLoaderData} from "react-router-dom"
 
 const Users = () => {
   const loadedUsers=useLoaderData()
+  const [users,setUsers]=useState(loadedUsers)
+
+  const handelDelete = id =>{
+    fetch(`http://localhost:5000/user/${id}`,{
+      method:'DELETE'
+    })
+    .then(res =>res.json())
+    .then(data =>{
+      if(data.deletedCount > 0){
+        console.log('deleted successfully')
+        const reamainingUser=users.filter(user =>user._id !==id)
+        setUsers(reamainingUser)
+      }
+    })
+  }
   return (
     <div>
       <h2>users {loadedUsers.length}</h2>
@@ -19,11 +35,15 @@ const Users = () => {
     <tbody>
       {/* row 1 */}
     {
-      loadedUsers.map(user =>   <tr key={user._id}>
+      users.map(user =>   <tr key={user._id}>
         <th>1</th>
         <td>{user.email}</td>
         <td>{user.createdAt}</td>
-        <td>Blue</td>
+        <td></td>
+        <td>
+
+          <button onClick={() => handelDelete(user._id)} className="btn">X</button>
+        </td>
       </tr>)
     }
     </tbody>
